01cbf60b02ee584e3cecf3b927b050fa58983cf0
Shafi <jonyszone@gmail.com>
2023-06-12 06:47:06 +0600

    fix incompatible class error

--- app/src/main/java/com/example/jetpackapplicationresources/MainActivity.kt
+++ app/src/main/java/com/example/jetpackapplicationresources/MainActivity.kt
@@ -3,19 +3,28 @@ package com.example.jetpackapplicationresources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -25,31 +34,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-
-import android.util.Log
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.*
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 import com.example.jetpackapplicationresources.ui.theme.JetPackApplicationResourcesTheme
 
 class MainActivity : ComponentActivity() {
@@ -168,8 +159,7 @@ fun Portfolio(data: List<String>) {
                 modifier = Modifier
                     .padding(13.dp)
                     .fillMaxWidth(),
-                shape = RectangleShape,
-                elevation = 4.dp
+                shape = RectangleShape
             ) {
                 Row(
                     modifier = Modifier
@@ -206,14 +196,14 @@ fun Portfolio(data: List<String>) {
 private fun CreateInfo() {
     Column(modifier = Modifier.padding(5.dp)) {
         Text(
-            text = "Miles P.",
+            text = "Shafi Ul Islam",
             style = MaterialTheme.typography.headlineMedium,
             color = MaterialTheme.colorScheme.primary
         )
         Text(text = "Android Compose Programmer", modifier = Modifier.padding(3.dp))
 
         Text(
-            text = "@themesCompose",
+            text = "jonyszone@gmail.com",
             modifier = Modifier.padding(3.dp),
             style = MaterialTheme.typography.titleMedium
         )
@@ -229,9 +219,7 @@ private fun CreateImageProfile(modifier: Modifier = Modifier) {
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
-        border = BorderStroke(0.5.dp, Color.LightGray),
-        elevation = 4.dp,
-        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+        border = BorderStroke(0.5.dp, Color.LightGray)
     ) {
 
         Image(
--- app/src/main/java/com/example/jetpackapplicationresources/ui/theme/Shape.kt
+++
@@ -1,2 +0,0 @@
-package com.example.jetpackapplicationresources.ui.theme
-
